09e927e73a76d60b11a8db52933fda387a667b28
even098 <143206114+even098@users.noreply.github.com>
2025-06-06 19:56:20 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$RAILWAY_SERVICE_NAME" = "web" ]; then
   python manage.py collectstatic --noinput
   python manage.py runserver 0.0.0.0:8000
 elif [ "$RAILWAY_SERVICE_NAME" = "celery" ]; then
-  celery -A currency_monitoring worker --loglevel=info
+  celery -A currency_monitoring worker --loglevel=info &
   celery -A currency_monitoring beat --loglevel=info
 elif [ "$RAILWAY_SERVICE_NAME" = "web+bot" ]; then
   # запускаем web и bot параллельно
