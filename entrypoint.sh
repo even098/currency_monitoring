42c222986df475e35740578997e0c3c198cf93f8
even098 <143206114+even098@users.noreply.github.com>
2025-06-06 19:54:26 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-set -e
-
-# Применим миграции перед запуском любого сервиса, кроме Celery Worker
-if [ "$1" != "worker" ]; then
-    python manage.py migrate
+if [ "$RAILWAY_SERVICE_NAME" = "web" ]; then
+  python manage.py migrate
+  python manage.py collectstatic --noinput
+  python manage.py runserver 0.0.0.0:8000
+elif [ "$RAILWAY_SERVICE_NAME" = "worker" ]; then
+  celery -A currency_monitoring worker --loglevel=info
+elif [ "$RAILWAY_SERVICE_NAME" = "beat" ]; then
+  celery -A currency_monitoring beat --loglevel=info
+elif [ "$RAILWAY_SERVICE_NAME" = "web+bot" ]; then
+  # запускаем web и bot параллельно
+  python manage.py runserver 0.0.0.0:8000 &
+  python bot/main.py
 fi
-
-case "$1" in
-  web)
-    python manage.py runserver 0.0.0.0:8000
-    ;;
-  bot)
-    python bot/main.py
-    ;;
-  worker)
-    celery -A currency_monitoring worker --loglevel=info
-    ;;
-  beat)
-    celery -A currency_monitoring beat --loglevel=info
-    ;;
-  *)
-    exec "$@"
-    ;;
-esac
