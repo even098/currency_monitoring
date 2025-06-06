93e54def193f7bd891591737f5159cf1dc5ad483
even098 <143206114+even098@users.noreply.github.com>
2025-06-06 19:56:02 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@ if [ "$RAILWAY_SERVICE_NAME" = "web" ]; then
   python manage.py migrate
   python manage.py collectstatic --noinput
   python manage.py runserver 0.0.0.0:8000
-elif [ "$RAILWAY_SERVICE_NAME" = "worker" ]; then
+elif [ "$RAILWAY_SERVICE_NAME" = "celery" ]; then
   celery -A currency_monitoring worker --loglevel=info
-elif [ "$RAILWAY_SERVICE_NAME" = "beat" ]; then
   celery -A currency_monitoring beat --loglevel=info
 elif [ "$RAILWAY_SERVICE_NAME" = "web+bot" ]; then
   # запускаем web и bot параллельно
